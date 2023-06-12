4eaa94e6ffa49028acd73ed0d663cef88c337cea
João Otávio <joaotavioos@outlook.com>
2023-06-12 07:40:47 -0300

    feat: add rateLimit for /api/Simulacao ✨

--- framework/routes/routes.go
+++ framework/routes/routes.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	"time"
+
+	ratelimit "github.com/JGLTechnologies/gin-rate-limit"
 	"github.com/gin-gonic/gin"
 	"github.com/hack-caixa/application/services"
 	"github.com/hack-caixa/framework/middlewares"
@@ -11,10 +14,28 @@ func SetupRouter(r *gin.Engine, db *gorm.DB) *gin.Engine {
 
 	r.Use(middlewares.CORS())
 
+	store := ratelimit.InMemoryStore(&ratelimit.InMemoryOptions{
+		Rate:  time.Second,
+		Limit: 5,
+	})
+
+	ratelimit := ratelimit.RateLimiter(store, &ratelimit.Options{
+		ErrorHandler: errorHandler,
+		KeyFunc:      keyFunc,
+	})
+
 	api := r.Group("/api/Simulacao")
 	{
-		api.POST("/", func(ctx *gin.Context) { services.MakeSimulation(ctx, db) })
+		api.POST("/", ratelimit, func(ctx *gin.Context) { services.MakeSimulation(ctx, db) })
 	}
 
 	return r
+}
+
+func keyFunc(c *gin.Context) string {
+	return c.ClientIP()
+}
+
+func errorHandler(c *gin.Context, info ratelimit.Info) {
+	c.String(429, "Too many requests. Try again in "+time.Until(info.ResetTime).String())
 }
