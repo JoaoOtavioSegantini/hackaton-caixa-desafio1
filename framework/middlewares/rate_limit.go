aad81229104253e5701398edfc92d05cdee5a2fc
João Otávio <joaotavioos@outlook.com>
2023-06-12 07:52:14 -0300

    refactor: change rateLimit organization 📦

---
+++ framework/middlewares/rate_limit.go
@@ -0,0 +1,31 @@
+package middlewares
+
+import (
+	"time"
+
+	ratelimit "github.com/JGLTechnologies/gin-rate-limit"
+	"github.com/gin-gonic/gin"
+)
+
+func RateLimit() gin.HandlerFunc {
+	store := ratelimit.InMemoryStore(&ratelimit.InMemoryOptions{
+		Rate:  time.Second,
+		Limit: 5,
+	})
+
+	ratelimit := ratelimit.RateLimiter(store, &ratelimit.Options{
+		ErrorHandler: errorHandler,
+		KeyFunc:      keyFunc,
+	})
+
+	return ratelimit
+
+}
+
+func keyFunc(c *gin.Context) string {
+	return c.ClientIP()
+}
+
+func errorHandler(c *gin.Context, info ratelimit.Info) {
+	c.String(429, "Too many requests. Try again in "+time.Until(info.ResetTime).String())
+}
--- framework/routes/routes.go
+++ framework/routes/routes.go
@@ -1,9 +1,6 @@
 package routes
 
 import (
-	"time"
-
-	ratelimit "github.com/JGLTechnologies/gin-rate-limit"
 	"github.com/gin-gonic/gin"
 	"github.com/hack-caixa/application/services"
 	"github.com/hack-caixa/framework/middlewares"
@@ -13,29 +10,12 @@ import (
 func SetupRouter(r *gin.Engine, db *gorm.DB) *gin.Engine {
 
 	r.Use(middlewares.CORS())
-
-	store := ratelimit.InMemoryStore(&ratelimit.InMemoryOptions{
-		Rate:  time.Second,
-		Limit: 5,
-	})
-
-	ratelimit := ratelimit.RateLimiter(store, &ratelimit.Options{
-		ErrorHandler: errorHandler,
-		KeyFunc:      keyFunc,
-	})
+	rateLimit := middlewares.RateLimit()
 
 	api := r.Group("/api/Simulacao")
 	{
-		api.POST("/", ratelimit, func(ctx *gin.Context) { services.MakeSimulation(ctx, db) })
+		api.POST("/", rateLimit, func(ctx *gin.Context) { services.MakeSimulation(ctx, db) })
 	}
 
 	return r
-}
-
-func keyFunc(c *gin.Context) string {
-	return c.ClientIP()
-}
-
-func errorHandler(c *gin.Context, info ratelimit.Info) {
-	c.String(429, "Too many requests. Try again in "+time.Until(info.ResetTime).String())
 }
